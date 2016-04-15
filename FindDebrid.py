aab72e2f362429daa9f5dada0240e3506a0dc32e
Glenn Abastillas <A5RJQZZ@usac.mmm.com>
2016-04-15 09:10:09 -0400

    Added web and find debride changes

--- FindDebrid.py
+++ FindDebrid.py
@@ -2,71 +2,131 @@ from pyDocs.DocumentPlus import DocumentPlus
 import os
 from Clients import Clients
 
-home = u"C:\\Users\\a5rjqzz\\Desktop\\Python\\files\\mcs-angina-All-2.txt"
+home = u"C:\\Users\\a5rjqzz\\Desktop\\Python\\files\\mcs-angina-test-3.txt"
 
 #clientList = Clients().get("stjoes", "dallas", "parkland", "tanner", "beaufort", "halifax", "stmarys", "baylor")
-clientList = Clients().getAll()
+#clientList = Clients().get("mcs")
 #clientList = Clients().get("mcs")
 
 documentPlus = DocumentPlus()
+getWords 	= documentPlus.getWords
+openDoc 	= documentPlus.open
+findLines	= documentPlus.findLines
+#findTokens	= documentPlus.findTokens
+
+terms = ["myocardial infarction", "mi", "angina", "heart attack", "coronary syndrome", "acs", "prinzmetal", "de novo", "ics", "stemi", "nstemi", "nonstemi", "non-stemi", "non stemi"]
+#terms = ["Myocardial infarction"]
+
+wordCounts = 0
+docCounts = 0
+
+#<><><><><><><><><><><><><><><><><
+# Get a list of paths
+#<><><><><><><><><><><><><><><><><
+print(">> Getting a list of paths")
+#documents = Clients().getFiles("stjoes", "dallas")
+documents = Clients().getAllFiles()
+
+print(">> Getting a list of documents")
+documentNames= ([document.split('\\')[-1]] for document in documents)
+documentList = (openDoc(document) for document in documents)
+print(">> Getting counts")
+docCounts	 = sum((1 for document in documents))
+wordCounts	 = sum((len(openDoc(document)) for document in documents))
+
+# extendDocList= documentList.extend
+# appendToNames= documentNames.append
+
+"""
+for document in documents:
+	openedDocument = openDoc(document)
+	wordCounts	  += len(getWords(openedDocument))
+	docCounts	  += 1
+
+	appendToNames([document])
+	extendDocList(openedDocument)
+
+print "\t>> wordCounts", wordCounts
+print "\t>> docCounts", docCounts
+"""
+
+#print openedDocumentList
+#<><><><><><><><><><><><><><><><><
+# Do the work
+#<><><><><><><><><><><><><><><><><
+print(">> Getting a list of excerpts")
 matches 	 = list()
-tokens	 	 = list()
-documentNames= list()
+#tokens	 	 = list()
 append	 	 = matches.append
-appendTokens = tokens.append
-appendDocName= documentNames.append
+#appendTokens = tokens.append
 
-term = "angina"
-countWords = 0.0
-countDocs = 0.0
 
-# Loop through the client sites
-for client in clientList:
-	#--x documents = os.listdir(client)
-	documents = set(["{0}\\{1}".format(client, item) for item in os.listdir(client) if not os.path.isdir("{0}\\{1}".format(client, item))])
+for document in documentList:
 
+	for term in terms:
+		
+		#print "\tSearching for term: {0}".format(term)
+		resultsFound = findLines(document, term, 50)
+
+		if len(resultsFound) > 0:
+			append(resultsFound)
+			#print "Found result: ", resultsFound
+
+"""
+	print "Working on [{1} documents] {0}".format(client, len(documents))
 	# Loop through the client site's documents
 	for document in documents:
 		
-		openedDocument = documentPlus.open(document)
+		openedDocument = openDoc(document)
 
-		forCount = documentPlus.getWords(openedDocument)
+		forCount = getWords(openedDocument)
 		
 		countWords += len(forCount)
-		countDocs += 1.0
+		countDocs  += 1.0
 		#print countWords
 		#print countDocs
 		#print forCount
 		#print document.split('\\')[-1],'\t', document
 		#break
 		#break
-		append(documentPlus.findLines(openedDocument, term, 50))
-		appendTokens(documentPlus.findTokens(openedDocument, term))
-		appendDocName(document.split('\\')[-1])
+
+		#--X append(documentPlus.findLines(openedDocument, term, 50))
+		#--X appendTokens(documentPlus.findTokens(openedDocument, term))
+		#--X appendDocName(document.split('\\')[-1])
+
+		for term in terms:
+			append(findLines(openedDocument, term, 50))
+			appendTokens(findTokens(openedDocument, term))
+			appendDocName(document.split('\\')[-1])
 		#print documentNames
 		
-	break
+	#break
+"""
 
 output = ""
-results = zip(matches, tokens, documentNames)
-hits = 0.0
-for match, token, documentName in results:
+#results = zip(matches, tokens, documentNames)
+results = zip(matches, documentNames)
+hits = 0
+#for match, documentName in results:
+for match, documentName in results:
 
 	if len(match) > 0:
-		subResults = zip(match, token, [documentName])
-		hits += 1.0
-		print "sub results", subResults
-		for matchTerm, tokenTerm, docName in subResults:
-			output += "{2}\t{0}\t{1}\n".format(matchTerm.replace('\n', ''), tokenTerm, docName)
-			break
+		subResults = zip(match, documentName)
+		#subResults = match
+		hits += 1
+		#print "sub results", subResults
+		for matchTerm, document in subResults:
+			output += "{0}\t{1}\n".format(document, matchTerm.replace('\n', ''))
+			#break
 	
-#documentPlus.save(home, output)
-
+documentPlus.save(home, output)
+print matches[:10]
+print results[:2]
 #hits = [1 for match in matches if len(match) > 0]
 
-print "Documents from {0}".format(clientList)
-print "Percent of Corpus:\t{0}%".format((hits/countWords)*100)
+#print "Documents from {0}".format(clientList)
+print "Percent of Corpus:\t{0}%".format((round(hits/wordCounts)*100))
 print "Number in Corpus:\t{0}".format(hits)
-print "Corpus Size:\t{0}".format(countWords)
-print "Number of Documents:\t{0}".format(countDocs)
-print "Mean Words per Document:\t{0}".format((hits/countDocs)*100)
+print "Corpus Size:\t{0}".format(wordCounts)
+print "Number of Documents:\t{0}".format(docCounts)
+print "Mean Words per Document:\t{0}".format(round(hits/docCounts))
---
+++ pyWeb/Web.py
@@ -0,0 +1,27 @@
+
+import urllib2
+import os
+
+class Web(object):
+
+	def __init__(self):
+		pass
+
+	def get(self, url=None):
+		
+		pass
+
+	def getPages(self, *urls):
+		pass
+
+	def method3(self):
+		pass
+
+	def method4(self):
+		pass
+
+	def method5(self):
+		pass
+
+	def method6(self):
+		pass
